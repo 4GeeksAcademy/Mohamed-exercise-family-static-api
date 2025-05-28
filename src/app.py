3afff26b7f3451518d098ca83ce57ada4fc67e0b
Mohamed Lemine tedjih <59097385+Med-lemin@users.noreply.github.com>
2025-05-28 17:54:36 +0000

    End Points

--- src/app.py
+++ src/app.py
@@ -28,14 +28,43 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-
+# Endpoint for all family members
 @app.route('/members', methods=['GET'])
 def handle_hello():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {"hello": "world",
                      "family": members}
-    return jsonify(response_body), 200
+    return jsonify(response_body), 200 
+
+# Endpoint to get a single member 
+@app.route('/members/<int:memebr_id>',methods=['GET'])
+def get_member(member_id):
+    member=jackson_family.get_member(member_id) 
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
+
+# Endpoint to add a new member
+@app.route("/members",methods=['POST'])   
+def add_member():    
+    body = request.get_json()
+    if body:
+        new_member = jackson_family.add_member(body)
+        return jsonify(new_member), 201
+    else:
+        return jsonify({"error": "Invalid data"}), 400
+    
+# Endpoint to delete a member 
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    deleted = jackson_family.delete_member(member_id)
+    if deleted:
+        return jsonify({"done": "successfully deleted"}), 200
+    return jsonify({"error": "Member not found"}), 404
+
+
 
 
 
